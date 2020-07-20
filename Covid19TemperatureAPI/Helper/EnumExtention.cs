fea3ef3cbf42c5a2bc75fd288684d008e26bce09
Abhishek Thakur <abhishek.thakur@orange.com>
2020-07-20 12:16:47 +0800

    Login/Logout API

---
+++ Covid19TemperatureAPI/Helper/EnumExtention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Covid19TemperatureAPI.Helper
+{
+    public static class EnumExtention
+    {
+        public static string DisplayName(this Enum enumValue)
+        {
+            var enumType = enumValue.GetType();
+
+            var enumString = enumType.GetField(enumValue.ToString()).Name;
+
+            return (enumType.GetField(enumValue.ToString()).GetCustomAttribute<DisplayAttribute>()?.Name) ?? enumString;
+        }
+    }
+}
