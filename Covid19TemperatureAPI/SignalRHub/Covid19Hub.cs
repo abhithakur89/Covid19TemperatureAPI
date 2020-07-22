6da488f34772e6cad410d47fcfdc9f10c53f585a
Abhishek Thakur <abhishek.thakur@orange.com>
2020-07-22 14:33:47 +0800

    Added EmployeeID in high temperature notification

--- Covid19TemperatureAPI/SignalRHub/Covid19Hub.cs
+++ Covid19TemperatureAPI/SignalRHub/Covid19Hub.cs
@@ -1,14 +1,70 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Covid19TemperatureAPI.Entities.Data;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Covid19TemperatureAPI.SignalRHub
 {
     public class Covid19Hub : Hub<ICovidHubClient>
     {
+        private IConfiguration Configuration;
+        private readonly ILogger<Covid19Hub> _logger;
+        private ApplicationDbContext DbContext { get; set; }
+
+        public Covid19Hub(IConfiguration configuration, ApplicationDbContext applicationDbContext, ILogger<Covid19Hub> logger)
+        {
+            Configuration = configuration;
+            DbContext = applicationDbContext;
+            _logger = logger;
+        }
+
         public async Task BroadcastMessage(string message)
         {
-            await Clients.All.ReceiveMessage(message);
+            var notifiationMsgRecieved = new { Notification = string.Empty };
+            notifiationMsgRecieved = JsonConvert.DeserializeAnonymousType(message, notifiationMsgRecieved);
+
+            string notificationMessageToSend = message;
+
+            if (notifiationMsgRecieved.Notification == "Abnormal Temperature")
+            {
+                var abnormalTemperatureNotifiationMsg = new {
+                    Notification = string.Empty,
+                    Temperature = string.Empty,
+                    DeviceId = string.Empty,
+                    EmployeeId = string.Empty,
+                    PersonName = string.Empty,
+                    PersonId = string.Empty,
+                    Desc = string.Empty,
+                    Timestamp = string.Empty
+                };
+
+                abnormalTemperatureNotifiationMsg = JsonConvert.DeserializeAnonymousType(message, abnormalTemperatureNotifiationMsg);
+
+                var details = DbContext.Employees
+                    .Where(x => x.UID == abnormalTemperatureNotifiationMsg.PersonId.Trim())
+                    ?.Select(x => new { x.EmployeeId })
+                    ?.FirstOrDefault();
+
+                var abnormalTemperatureNotifiationMsgWithEmployeeInfo = new
+                {
+                    abnormalTemperatureNotifiationMsg.Notification,
+                    abnormalTemperatureNotifiationMsg.Temperature,
+                    abnormalTemperatureNotifiationMsg.DeviceId,
+                    EmployeeId = details?.EmployeeId ?? string.Empty,
+                    abnormalTemperatureNotifiationMsg.PersonName,
+                    abnormalTemperatureNotifiationMsg.PersonId,
+                    abnormalTemperatureNotifiationMsg.Desc,
+                    abnormalTemperatureNotifiationMsg.Timestamp
+                };
+
+                notificationMessageToSend = JsonConvert.SerializeObject(abnormalTemperatureNotifiationMsgWithEmployeeInfo);
+            }
+
+            await Clients.All.ReceiveMessage(notificationMessageToSend);
         }
 
         public async Task SendToCaller(string message)
