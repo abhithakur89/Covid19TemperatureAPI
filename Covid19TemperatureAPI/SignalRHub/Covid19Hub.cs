9fa0ef55fef27c401b438f8d626f71bfc250e30d
Abhishek Thakur <abhishek.thakur@orange.com>
2020-07-24 11:02:34 +0800

    Generic notification

--- Covid19TemperatureAPI/SignalRHub/Covid19Hub.cs
+++ Covid19TemperatureAPI/SignalRHub/Covid19Hub.cs
@@ -25,89 +25,103 @@ namespace Covid19TemperatureAPI.SignalRHub
 
         public async Task BroadcastMessage(string message)
         {
-            var notifiationMsgRecieved = new { Notification = string.Empty };
+            var notifiationMsgRecieved = new
+            {
+                Notification = string.Empty,
+                Value = string.Empty,
+                DeviceId = string.Empty,
+                EmployeeId = string.Empty,
+                PersonName = string.Empty,
+                PersonId = string.Empty,
+                Desc = string.Empty,
+                Timestamp = string.Empty
+            };
             notifiationMsgRecieved = JsonConvert.DeserializeAnonymousType(message, notifiationMsgRecieved);
 
-            string notificationMessageToSend = message;
+            //string notificationMessageToSend = message;
 
             // Prepare push message
-            if (notifiationMsgRecieved.Notification == "Abnormal Temperature")
+            if (notifiationMsgRecieved.Notification == Configuration["TemperaturePolicyName"])
             {
-                var abnormalTemperatureNotifiationMsg = new {
-                    Notification = string.Empty,
-                    Temperature = string.Empty,
-                    DeviceId = string.Empty,
-                    EmployeeId = string.Empty,
-                    PersonName = string.Empty,
-                    PersonId = string.Empty,
-                    Desc = string.Empty,
-                    Timestamp = string.Empty
-                };
+                // Get Config setting for temperature threshold
+                bool res = decimal.TryParse(DbContext.Configurations
+                    .Where(x => x.ConfigKey == "TemperatureThreshold")
+                    .Select(x => x.ConfigValue).FirstOrDefault(), out decimal thresholdTemperature);
 
-                abnormalTemperatureNotifiationMsg = JsonConvert.DeserializeAnonymousType(message, abnormalTemperatureNotifiationMsg);
+                if(!res)
+                    decimal.TryParse(Configuration["DefaultTemperatureThreshold"], out thresholdTemperature);
 
-                var details = DbContext.Employees
-                    .Where(x => x.UID == abnormalTemperatureNotifiationMsg.PersonId.Trim())
-                    ?.Select(x => new { x.EmployeeId })
-                    ?.FirstOrDefault();
+                // Parse received teperature
+                res = decimal.TryParse(notifiationMsgRecieved.Value, out decimal receivedTeperature);
 
-                var abnormalTemperatureNotifiationMsgWithEmployeeInfo = new
+                if(res)
                 {
-                    abnormalTemperatureNotifiationMsg.Notification,
-                    abnormalTemperatureNotifiationMsg.Temperature,
-                    abnormalTemperatureNotifiationMsg.DeviceId,
-                    EmployeeId = details?.EmployeeId ?? string.Empty,
-                    abnormalTemperatureNotifiationMsg.PersonName,
-                    abnormalTemperatureNotifiationMsg.PersonId,
-                    abnormalTemperatureNotifiationMsg.Desc,
-                    abnormalTemperatureNotifiationMsg.Timestamp
-                };
+                    // Check threshold
+                    if(receivedTeperature>= thresholdTemperature)
+                    {
+                        // Notify everyone
+                        await Clients.All.ReceiveMessage(Configuration["HighTemperatureAlertNotificationString"]);
 
+                        #region SMS
+                        // Notify SMS
+                        var sendAlertSMSConfig = DbContext.Configurations
+                            .Where(x => x.ConfigKey == "SendAlertSMS")
+                            .Select(x => x.ConfigValue)
+                            .FirstOrDefault();
+                        if(sendAlertSMSConfig=="1")
+                        {
+                            // Prepare SMS
+                            string smsBody = DbContext.Configurations
+                                .Where(x => x.ConfigKey == "TemperatureAlertHeader")
+                                .Select(x => x.ConfigValue)
+                                .FirstOrDefault();
 
-                // Final Push Message
-                notificationMessageToSend = JsonConvert.SerializeObject(abnormalTemperatureNotifiationMsgWithEmployeeInfo);
+                            // Get SMS sender
+                            string smsSender = DbContext.Configurations
+                                .Where(x => x.ConfigKey == "SMSSender")
+                                .Select(x => x.ConfigValue)
+                                .FirstOrDefault();
 
-                // Push all clients
-                await Clients.All.ReceiveMessage(notificationMessageToSend);
-
-                // Prepare SMS
-                string smsBody = DbContext.Configurations
-                    .Where(x => x.ConfigKey == "Abnormal_Temperature_SMS_Body")
-                    .Select(x => x.ConfigValue)
-                    .FirstOrDefault();
-
-                // Get SMS sender
-                string smsSender = DbContext.Configurations
-                    .Where(x => x.ConfigKey == "SMSSender")
-                    .Select(x => x.ConfigValue)
-                    .FirstOrDefault();
-
-
-                // Get mobile numbers for this site 
-                var v = from a in DbContext.Devices
+                            // Get mobile numbers for this site 
+                            var v = from a in DbContext.Devices
                                     join b in DbContext.Gates on a.GateId equals b.GateId
                                     join c in DbContext.AlertMobileNumbers on b.Floor.Building.SiteId equals c.SiteId
-                                    where a.DeviceId == abnormalTemperatureNotifiationMsg.DeviceId
+                                    where a.DeviceId == notifiationMsgRecieved.DeviceId
                                     select new
                                     {
-                                        c.MobileNumber, b.Floor.Building.BuildingName, b.GateNumber
+                                        c.MobileNumber,
+                                        b.Floor.Building.BuildingName,
+                                        b.GateNumber
                                     };
 
-                // Send SMS 
-                foreach (var v1 in v)
-                {
-                    var body = smsBody + $"\nName - {abnormalTemperatureNotifiationMsg.PersonName}\n" +
-                        $"Temperature - {abnormalTemperatureNotifiationMsg.Temperature}\n" +
-                        $"Building - {v1.BuildingName}\n" +
-                        $"Gate - {v1.GateNumber}\n" +
-                        $"Time - {Convert.ToDateTime(abnormalTemperatureNotifiationMsg.Timestamp).ToString("MMMM dd hh:mm tt")}";
+                            // Send SMS 
+                            foreach (var v1 in v)
+                            {
+                                var body = smsBody + $"\nName - {notifiationMsgRecieved.PersonName}\n" +
+                                    $"Temperature - {notifiationMsgRecieved.Value}\n" +
+                                    $"Building - {v1.BuildingName}\n" +
+                                    $"Gate - {v1.GateNumber}\n" +
+                                    $"Time - {Convert.ToDateTime(notifiationMsgRecieved.Timestamp).ToString("MMMM dd hh:mm tt")}";
 
-                    SMSSender.SendSMS(ApiKey: Configuration["NexmmoApiKey"],
-                        ApiSecret: Configuration["NexmoApiSecret"],
-                        from: smsSender,
-                        to: v1.MobileNumber,
-                        msg: body);
+                                SMSSender.SendSMS(ApiKey: Configuration["NexmmoApiKey"],
+                                    ApiSecret: Configuration["NexmoApiSecret"],
+                                    from: smsSender,
+                                    to: v1.MobileNumber,
+                                    msg: body);
+                            }
+                        }
+                        #endregion
+                        // Notify Email
+                        // ...
+                    }
                 }
+
+                await Clients.All.ReceiveMessage(Configuration["TemperatureEventNotificationString"]);
+            }
+
+            if (notifiationMsgRecieved.Notification == Configuration["NoMaskPolicyName"])
+            {
+                await Clients.All.ReceiveMessage(Configuration["TemperatureEventNotificationString"]);
             }
         }
     }
